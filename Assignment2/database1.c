310e6224d378a218e4594556e7b3ae936b9bbdfb
minez991 <peterfu00@gmail.com>
2018-04-27 15:51:53 +0100

    27/4/2018
    Part 1 Finished tree insert, and print data base finished. Verification need to be done.

--- Assignment2/database1.c
+++ Assignment2/database1.c
@@ -1,22 +1,45 @@
 #include "database_main.h"
 
+void print_book(struct Book* book){
 
-void addtotree(struct Book* book_tree,struct Book* newbook){
+	if (book == NULL){
+	return;
+	}
+	print_book(book->left);
+	printf("Title: %s\n",book->title);
+	printf("Author: %s\n",book->author);
+	printf("Year: %i\n\n",book->year);
+	print_book(book->right);
+
+	/*if (book->left != NULL){
+		print_book(book->left);
+
+	}else{
+		fprintf( stderr,"left: NULL\n");
+
+	}
+	if (book->right != NULL){
+		print_book(book->right);
+	}else{
+		fprintf( stderr,"right: NULL\n");
+	}*/
+
+}
+
+
+void addtotree(struct Book** book_tree,struct Book* newbook){
 	/*if the book tree is empty add this in*/
+	if (*book_tree == NULL){
 
-	if (book_tree == NULL){
+		*book_tree = newbook;
 
-		book_tree = newbook;
-		printf("\n%s\n",newbook->title );
-		printf("%s\n",book_tree->title );
+	}else if(strcmp(newbook->title,(*book_tree)->title) > 0 ){
 
-	}else if(strcmp(newbook->title,book_tree->title) > 0 ){
+		addtotree((&(*book_tree)->right),newbook);
 
-		addtotree(book_tree->right,newbook);
+	}else if(strcmp(newbook->title,(*book_tree)->title) < 0 ){
 
-	}else if(strcmp(newbook->title,book_tree->title) < 0 ){
-
-		addtotree(book_tree->left,newbook);
+		addtotree((&(*book_tree)->left),newbook);
 	}
 
 };
@@ -25,7 +48,9 @@ void addtotree(struct Book* book_tree,struct Book* newbook){
  *
  * Add new book to database
  */
+
 void menu_add_book(void){
+	char c;
 	struct Book *new;
 	new = (struct Book*) malloc(sizeof(struct Book));
 	fprintf ( stderr,"Book Name:");
@@ -33,17 +58,16 @@ void menu_add_book(void){
 	fprintf ( stderr,"Author: ");
 	scanf ("%[^\n]%*c", new->author);
 	fprintf ( stderr,"Year: ");
-	scanf ("%i", &(new->year);	
-	fprintf(stderr,"Year = %i",new->year);
+	scanf ("%i", &(new->year));	
+	while ((c = getchar()) != '\n' && c != EOF) { }
 	new->left=new->right=NULL;
-	addtotree(book_tree,new);
+	addtotree(&book_tree,new);
 }
 
 
 /* menu_print_database():
- *
  * Print database of books to standard output in alphabetical order of title.
  */
 void menu_print_database(void){
-   /* fill in the code here in part 1, and add any extra functions you need */
+   print_book(book_tree);
 }	    
